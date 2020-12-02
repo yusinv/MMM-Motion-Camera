cf840d78fb8770fd3f18dd3591e9fde02caa3539
Valentin <yusinv@gmail.com>
2020-12-02 22:02:36 +0300

    change logic to use rtsp stream instead of mjpeg

--- MMM-Motion-Camera.js
+++ MMM-Motion-Camera.js
@@ -2,95 +2,104 @@
 
 /* Magic Mirror
  * Module: MMM-Motion-Camera
- *
- * By Valentin
- * MIT Licensed.
  */
 
 Module.register("MMM-Motion-Camera", {
-	defaults: {
-		height: "300px",
-		width: "100%",
-		scrolling: "no",
-		notification_label: "MOTION_DETECTED",
-		cameraUrl: "http://door/picture/1/frame/",
-	},
+    defaults: {
+        animationSpeed: 1500,
+        stream1: {
+            streamUrl: "rtsp://door_camera/live/ch00_2",
+            playerWidth: 800,
+            playerHeight: 450
+        }
+    },
 
-	requiresVersion: "2.1.0", // Required version of MagicMirror
+    requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function () {
+    start: function () {
+        this.motionDetected = false;
+        this.sendSocketNotification("INIT", this.config);
+    },
 
-		//let self = this;
-		console.log("socketNotificationReceived2");
-		//send message to establish connection with helper
-		//this.sendSocketNotification("main", {cameraId: this.config.cameraId});
-		this.motionDetected = false;
-	},
+    getDom: function () {
+        // create element wrapper for show into the module
+        const wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
 
-	getDom: function () {
-		var self = this;
+        const frameDiv = document.createElement("div");
+        frameDiv.id = "camera_frame"
+        wrapper.appendChild(frameDiv);
 
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		wrapper.className = "wrapper";
-
-		var frameDiv = document.createElement("div");
-
-		var iFrame = document.createElement("IFRAME");
-		iFrame.className = "iframe";
-		iFrame.src = self.config.cameraUrl;
-
-
-		if (this.motionDetected) {
-			wrapper.className = "wrapper_motion";
-			var label = document.createElement("label");
-			label.innerHTML = this.translate("MOTION");
-			label.className = "label";
-			wrapper.appendChild(label);
-			wrapper.appendChild(document.createElement("br"));
-			frameDiv.className = "camera_motion";
-		} else {
-			frameDiv.className = "camera";
-
-		}
+        if (this.motionDetected) {
+            wrapper.className = "wrapper_motion";
+            const label = document.createElement("label");
+            label.innerHTML = this.translate("MOTION");
+            label.className = "label";
+            wrapper.appendChild(label);
+            wrapper.appendChild(document.createElement("br"));
+            frameDiv.className = "camera_motion";
+        } else {
+            frameDiv.className = "camera";
+        }
 
 
-		wrapper.appendChild(frameDiv);
-		frameDiv.appendChild(iFrame);
-		return wrapper;
-	},
+        wrapper.appendChild(frameDiv);
+        return wrapper;
+    },
 
-	getStyles: function () {
-		return [
-			"motion.css",
-		];
-	},
+    getStyles: function () {
+        return [
+            "motion.css",
+        ];
+    },
 
-	// Load translations files
-	getTranslations: function () {
-		//FIXME: This can be load a one file javascript definition
-		return {
-			en: "translations/en.json",
-			ru: "translations/ru.json"
-		};
-	},
+    // Load translations files
+    getTranslations: function () {
+        //FIXME: This can be load a one file javascript definition
+        return {
+            en: "translations/en.json",
+            ru: "translations/ru.json"
+        };
+    },
 
-	//socketNotificationReceived from helper
-	notificationReceived: function (notification, payload, sender) {
-		console.log("socketNotificationReceived" + notification);
-		Log.log("socketNotificationReceived2" + payload);
-		if (notification === this.config.notification_label) {
-			switch (payload) {
-			case "0":
-				this.motionDetected = false;
-				this.header = "noone";
-				break;
-			case "1":
-				this.motionDetected = true;
-				this.header = "AA!!!!!!";
-				break;
-			}
-			this.updateDom(100);
-		}
-	},
+
+    notificationReceived: function (notification, payload, sender) {
+        if (notification === "MOTION_START") {
+            this.motionDetected = true;
+            this.updateDom(this.config.animationSpeed);
+            this.sendSocketNotification("START_PLAYER", payload);
+        } else if (notification === "MOTION_END") {
+            this.motionDetected = false;
+            this.updateDom(this.config.animationSpeed);
+            this.sendSocketNotification("STOP_PLAYER", payload);
+        } else if (notification === "VIDEO_SNAPSHOT") {
+            this.sendSocketNotification("PREPARE_SNAPSHOT", payload);
+        }
+    },
+
+
+    // socketNotificationReceived from helper
+    socketNotificationReceived: function (notification, payload) {
+        if (notification === "STARTED") {
+            if (!this.loaded) {
+                this.loaded = true;
+                this.updateDom(this.config.animationSpeed);
+            }
+            // this.sendSocketNotification("MODE_SNAPSHOT", payload);
+        }
+        if (notification === "UPDATE_SNAPSHOT") {
+            if (payload.image && !this.motionDetected) {
+                let img = document.getElementById("camera_image");
+                if (img === null) {
+                    const cf = document.getElementById("camera_frame")
+                    img = document.createElement("img");
+                    img.style.width = "100%";
+                    img.style.height = "100%";
+                    img.id = "camera_image";
+                    cf.appendChild(img)
+                }
+                img.src = payload.buffer;
+            }
+        }
+    },
 });
---
+++ node_helper.js
@@ -0,0 +1,68 @@
+const NodeHelper = require("node_helper");
+const dataUriParser = require("datauri/parser");
+const childProcess = require("child_process");
+const environ = Object.assign(process.env, {DISPLAY: ":0"});
+
+module.exports = NodeHelper.create({
+
+    config: {},
+    snapshots: {},
+    players: {},
+
+    start: function () {
+        this.started = false;
+    },
+
+    stop: function () {
+        console.log("Shutting down MMM-Motion");
+        Object.keys(this.players).forEach(s => {
+            this.stopFFPlay(s);
+        });
+    },
+
+    stopFFPlay: function (name) {
+        console.log("Stopping player for stream " + name);
+        const proc = this.players[name];
+        proc.kill();
+    },
+
+    startFFPlay: function (name) {
+        const sc = this.config[name];
+        const cmd = "ffplay";
+        const args = [sc.streamUrl, "-x", sc.playerWidth, "-y", sc.playerHeight, "-v", "error"];
+        const opts = {detached: false, env: environ, stdio: ["ignore", "ignore", "pipe"]};
+
+        this.players[name] = childProcess.spawn(cmd, args, opts);
+
+        this.players[name].on("error", (err) => {
+            console.error(err)
+            console.error(`Failed to start player: ${this.players[name]}.`);
+        });
+    },
+
+    socketNotificationReceived: function (notification, payload) {
+        const self = this;
+        if (notification === "INIT") {
+            this.config = payload;
+            let streams = Object.keys(this.config).filter(key => key.startsWith("stream"));
+            streams.forEach((name) => {
+                // this.startListener(name);
+                this.sendSocketNotification("STARTED", name);
+            });
+        } else if (notification === "STOP_PLAYER") {
+            if (payload in this.players) {
+                this.stopFFPlay(payload);
+                delete this.players[payload];
+            }
+        } else if (notification === "START_PLAYER") {
+            if (!(payload in this.players)) {
+                this.startFFPlay(payload);
+            }
+        } else if (notification === "PREPARE_SNAPSHOT") {
+            const parser = new dataUriParser();
+            parser.format(".jpeg", payload);
+            // console.log(parser.content);
+            this.sendSocketNotification("UPDATE_SNAPSHOT", {name: "stream1", image: true, buffer: parser.content});
+        }
+    },
+});
